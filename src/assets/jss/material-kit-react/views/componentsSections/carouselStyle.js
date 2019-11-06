c35079e61ded8809b7be907c35df192b15efb740
Elise Dixon <elisemd@live.unc.edu>
2019-11-05 19:25:09 -0500

    Add files via upload

---
+++ src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
@@ -0,0 +1,11 @@
+import { container } from "assets/jss/mainStyle.js"
+
+const carouselStyle = {
+  container,
+  marginAuto: {
+    marginLeft: "auto !important",
+    marginRight: "auto !important"
+  }
+};
+
+export default carouselStyle;
